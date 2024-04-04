361a971ab04c4dc7e5853d3d4b9048b4a2376e3e
ravikirananaparthi <anaparthiravikiran20037@gmail.com>
2024-04-04 21:29:39 +0530

    fixing issues and debugging..

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,33 +3,15 @@ import jwt  from "jsonwebtoken";
 
 
 export const isAuthenticated = async (req, res, next) => {
-  try {
-    const { token } = req.cookies;
-
-    if (!token) {
-      return res.status(401).json({
-        success: false,
-        message: "Unauthorized: Token not provided",
-      });
-    }
-
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    const user = await User.findById(decoded._id);
-
-    if (!user) {
-      return res.status(401).json({
-        success: false,
-        message: "Unauthorized: User not found",
-      });
-    }
-
-    req.user = user;
-    next();
-  } catch (error) {
-    return res.status(401).json({
+  const { token } = req.cookies;
+console.log(token);
+  if (!token)
+    return res.status(404).json({
       success: false,
-      message: "Unauthorized: Invalid token",
+      message: "Login First",
     });
-  }
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+  req.user = await User.findById(decoded._id);
+  next();
 };
