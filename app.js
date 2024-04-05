aa5aa14b33fbc905526e90ee63c851077f772e56
ravikirananaparthi <anaparthiravikiran20037@gmail.com>
2024-04-05 21:01:42 +0530

    set frontend origins

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: [frontendOrigin],
+    origin: [frontendOrigin,'https://devfinds-frontend.vercel.app/'],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
